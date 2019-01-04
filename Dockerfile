1d0af83c59f0dc2ad60d17208ec68ff58d1edc10
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-01-04 21:20:29 +0100

    Add basic behaviour for mercure

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:11.5-alpine
 
-RUN mkdir -p /usr/src/as-customer
+RUN mkdir -p /usr/src/asCustomer
 
-WORKDIR /usr/src/as-customer
+WORKDIR /usr/src/asCustomer
 
 # Prevent the reinstallation of node modules at every changes in the source code
 COPY package.json yarn.lock ./
--- src/lib/Notifications.ts
+++ src/lib/Notifications.ts
@@ -1,17 +1,25 @@
 export default function() {
     Notification.requestPermission().then(() => {
         if (Notification.permission === 'granted') {
+
             navigator.serviceWorker.getRegistration().then((reg) => {
-                if (undefined === reg) {
-                    return;
+                const es = new EventSource('https://mercure.keepmeatable.dev/hub?topic=A17CF9AD-C964-4CB5-90C2-2BED8F8BCE67');
+                es.onmessage = e => {
+                    console.log(e);
+
+                    navigator.serviceWorker.getRegistration().then((reg) => {
+                        if (undefined === reg) {
+                            return;
+                        }
+
+                        const options = {
+                            body: 'You can go!',
+                            vibrate: [5000, 1000, 5000, 1000, 5000],
+                        };
+
+                        reg.showNotification('Table\'s ready!', options);
+                    });
                 }
-
-                const options = {
-                    body: 'You can go!',
-                    vibrate: [5000, 1000, 5000, 1000, 5000],
-                };
-
-                reg.showNotification('Table\'s ready!', options);
             });
         }
     });
